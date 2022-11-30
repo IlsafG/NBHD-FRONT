974511efd67d5b55cf66876358f87b1f8295b9f5
IlsafG <89460342+IlsafG@users.noreply.github.com>
2022-11-30 20:42:45 +0300

    form

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import './App.css';
 import {useEffect} from "react";
 import {useTelegram} from "./hooks/useTelegram";
 import Header from "./components/Header/Header";
-// import {Route, Routes} from 'react-router-dom'
-// import ProductList from "./components/ProductList/ProductList";
-// import Form from "./components/Form/Form";
+import {Route, Routes} from 'react-router-dom'
+import ProductList from "./components/ProductList/ProductList";
+import Form from "./components/Form/Form";
 
 
 
@@ -14,10 +14,14 @@ function App() {
    useEffect(()=>{
       tg.ready();
   }, []) 
+
   return (
     <div className="App">
       <Header/>
-      <button onClick={onToggleButton}>toggle</button>
+      <Routes>
+        <Route index element={ProductList}/>
+        <Route path={"form"} element={Form}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/ProductList/ProductList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './ProductList.css'
+
+const ProductList = () => {
+
+    return(
+        <div className="product">
+
+        </div>
+    )
+}
+
+export default ProductList
